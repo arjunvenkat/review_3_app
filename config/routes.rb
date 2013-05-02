5744246add619adfb683f5d6ac82ea84781aaa18
Instructor <instructor@Arjuns-MacBook-Air.local>
2013-05-02 12:15:21 -0500

    adds starter_resources gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,3 @@
 Review3App::Application.routes.draw do
-  # CREATE
-  get "/books/new", :controller => "books", :action => "new", :as => 'new_book'
-  post "/books", :controller => "books", :action => "create"
 
-  # READ
-  get '/books', :controller => 'books', :action => 'index', :as => 'books'
-  get '/books/:id', :controller => 'books', :action => 'show', :as => 'book'
-
-  # UPDATE
-  get '/books/:id/edit', :controller => 'books', :action => 'edit', :as => 'edit_book'
-  put '/books/:id', :controller => 'books', :action => 'update'
-
-  # DELETE
-  delete '/books/:id/delete', :controller => 'books', :action => 'destroy'
 end
