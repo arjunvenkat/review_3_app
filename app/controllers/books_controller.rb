a5d02e098ebde0561f44b3762bf0c2eba1047620
Instructor <instructor@Arjuns-MacBook-Air.local>
2013-05-02 12:16:24 -0500

    uses starter:resource to create a Book resource with a column of title

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,46 @@
+class BooksController < ApplicationController
+
+  def index
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.find_by_id(params[:id])
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new
+    @book.title = params[:title]
+    
+    if @book.save
+            redirect_to books_url
+          else
+      render 'new'
+    end
+  end
+
+  def edit
+    @book = Book.find_by_id(params[:id])
+  end
+
+  def update
+    @book = Book.find_by_id(params[:id])
+    @book.title = params[:title]
+    
+    if @book.save
+            redirect_to books_url
+          else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @book = Book.find_by_id(params[:id])
+    @book.destroy
+        redirect_to books_url
+      end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Review3App::Application.routes.draw do
 
+  # Routes for the Book resource:
+  # CREATE
+  get '/books/new', controller: 'books', action: 'new', as: 'new_book'
+  post '/books', controller: 'books', action: 'create'
+
+  # READ
+  get '/books', controller: 'books', action: 'index', as: 'books'
+  get '/books/:id', controller: 'books', action: 'show', as: 'book'
+
+  # UPDATE
+  get '/books/:id/edit', controller: 'books', action: 'edit', as: 'edit_book'
+  put '/books/:id', controller: 'books', action: 'update'
+
+  # DELETE
+  delete '/books/:id', controller: 'books', action: 'destroy'
+  #------------------------------
+
 end
