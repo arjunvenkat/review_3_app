6e5803f5c157edb2510f8fd092833cdb84223a1c
Instructor <instructor@Arjuns-MacBook-Air.local>
2013-05-02 12:13:23 -0500

    adds routes for a book resource

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Review3App::Application.config.secret_token = '3c07f39496497185189777b5b1951dcc0095aa56a9e04b478cf8bcb178ac8272d41ea5867228cd437660045f98575addcce6f38b307f022d8d6454bf286f889f'
